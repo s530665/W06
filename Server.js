03f8a80730cd551cfe173f9a05cac72c382723b2
Venkata Siva Sai Nadipeneni <31713727+s530665@users.noreply.github.com>
2017-10-05 19:10:13 -0500

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,51 @@
+var express = require('express')
+var app = express()
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
+var path = require('path')
+
+// on getting a request to /, do the following
+app.get('/', function (req, res) {
+  app.use(express.static(path.join(__dirname)))   
+  res.sendFile(path.join(__dirname, '../W06/assets', 'index.html'))
+})
+io.on('connection', function(socket){ 
+  socket.on('chatMessage', function(from, msg){
+       io.emit('chatMessage', from, msg)  
+  })
+  socket.on('notifyUser', function(user){
+       io.emit('notifyUser', user)  
+  })
+})
+
+// Listen for an application request on port 8081
+http.listen(8081, function () {
+  console.log('listening on http://127.0.0.1:8081/')
+})
+
+
+
+
+
+
+// 1. add two more requires at the end of the requires
+
+
+// 2. replace the inside lines of your app.get 
+
+
+// 3. Add the following function  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
